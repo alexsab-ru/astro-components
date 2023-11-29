50dab016dd6ee9fc88653a7df99e42a0a12c614f
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-11-29 11:01:17 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), alpinejs()],
+	integrations: [
+		tailwind(), 
+		alpinejs()
+	],
 });
