2e2140eb11cf6c70574d230453d413c36da206b2
Alexander Sabanov <alexsab@mail.ru>
2023-11-29 11:09:17 +0400

    upd const and json

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
-export const SITE_NAME = 'SITE NAME'; //Название сайта по умолчанию
-export const LEGAL_ENTITY = 'FULL NAME COMPANY'; // Юр лицо
-export const LEGAL_INN = 'INN_123456789'; // Юр лицо
-export const SITE_DESCR = 'SITE DESCRIPTION'; //Описание сайта по умолчанию
+export const SITE_NAME = 'Название сайта'; // Название сайта по умолчанию
+export const LEGAL_ENTITY = 'ООО «Юридическое название»'; // Юр лицо
+export const LEGAL_INN = '1234567890'; // ИНН
+export const SITE_DESCR = 'Официальный дилерский центр'; // Описание сайта по умолчанию
 
 export const LINKS_MENU = [
 	{url: '#online-calc', name: 'Онлайн расчет'},
