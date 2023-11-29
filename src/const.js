06a11ee6303cf8510e9428f3834411d88912ae9c
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-11-29 11:34:06 +0500

    Create const.js

---
+++ src/const.js
@@ -0,0 +1,11 @@
+export const SITE_NAME = 'SITE NAME'; //Название сайта по умолчанию
+export const LEGAL_ENTITY = 'FULL NAME COMPANY'; // Юр лицо
+export const LEGAL_INN = 'INN_123456789'; // Юр лицо
+export const SITE_DESCR = 'SITE DESCRIPTION'; //Описание сайта по умолчанию
+
+export const LINKS_MENU = [
+	{url: '#online-calc', name: 'Онлайн расчет'},
+	{url: '#advantages', name: 'Преимущества'},
+	{url: '#services', name: 'Услуги сервиса'},
+	{url: '#contacts', name: 'Контакты'},
+];
