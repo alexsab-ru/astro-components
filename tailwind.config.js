d4e867a0bb1fb76ee09137f9cacbc1f34c5b70bb
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-12-01 08:16:17 +0500

    remove max classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export const theme = {
   },
   screens: {
     ...defaultTheme.screens,
+    'xs': {'max': '430px'},
     's1024_1074': {'min': '1024px', 'max': '1074px'},
     's1280_1330': {'min': '1280px', 'max': '1330px'},
     's1536_1586': {'min': '1536px', 'max': '1586px'},
