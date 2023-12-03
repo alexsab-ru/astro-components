6b220c1f320d19c27500cc696018ae743dd825d4
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-12-03 08:39:33 +0500

    color.links module -> ColorLink DIR

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import './data.alpine';
 import './modules/slider';
 import './modules/scroll';
-import './modules/color.links';
+// import './modules/color.links';
 import './modules/map';
 import './modules/modals';
