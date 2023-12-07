c05a0c30dd6934af9f288f47d1036055dfcea0be
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-12-07 09:46:20 +0500

    change logic color.links

--- src/components/ColorLink/color.links.js
+++ src/components/ColorLink/color.links.js
@@ -6,17 +6,23 @@ document.querySelectorAll(".color-link").forEach(function (link) {
 		const dir = parent.getAttribute("data-dir");
 		const color = this.getAttribute("data-color");
 		const colorName = this.getAttribute("title");
-		parent.querySelectorAll(".color-link.active").forEach((el) => {
-			el.classList.remove("active");
-		});
-		parent.querySelectorAll(`[data-color="${color}"]`).forEach((el) => {
-			el.classList.add("active");
-		});
-		parent.querySelectorAll(".color-img-preview").forEach((el) => {
-			el.setAttribute("src", `img/${dir}/${color}.png`);
-		});
-		parent.querySelectorAll(".color-name").forEach((el) => {
-			el.textContent = colorName;
-		});
+		const carImagePath = this.getAttribute("data-car-image") ? this.getAttribute("data-car-image") : `img/${dir}/${color}.png`
+		let img = new Image();
+		img.onload = function () {
+			// console.log(`Изображение загружено, размеры ${img.width}x${img.height}`);
+			parent.querySelectorAll(".color-link.active").forEach((el) => {
+				el.classList.remove("active");
+			});
+			parent.querySelectorAll(`[data-color="${color}"]`).forEach((el) => {
+				el.classList.add("active");
+			});
+			parent.querySelectorAll(".color-img-preview").forEach((el) => {
+				el.setAttribute("src", carImagePath);
+			});
+			parent.querySelectorAll(".color-name").forEach((el) => {
+				el.textContent = colorName;
+			});
+		}
+		img.src = carImagePath;
 	});
 });
