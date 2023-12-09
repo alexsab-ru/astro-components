1f40bce2b0d181823a7e5c5eb2d2c4eea36b3f5b
Alexander Sabanov <alexsab@mail.ru>
2023-12-09 10:01:13 +0400

    upd cookie and form

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
 import './data.alpine';
 import './modules/scroll';
 import './modules/modals';
+
+import { connectForms, cookiecook } from '@alexsab-ru/scripts';
+cookiecook();
+connectForms('https://alexsab.ru/lead/test/', function() {
+});
