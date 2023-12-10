da3ea5d87cbbda1969dc767c4dd493de21f5d5da
Alexander Sabanov <alexsab@mail.ru>
2023-12-10 23:59:12 +0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://site.com',
 	integrations: [
 		tailwind(), 
+		sitemap(),
 		alpinejs()
 	],
 });
