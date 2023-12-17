7afbd65085dc828d0d0c8de25d63de4f6ee476f1
Alexander Sabanov <alexsab@mail.ru>
2023-12-17 12:51:56 +0400

    get grecaptcha response

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,29 @@ import { connectForms, cookiecook } from '@alexsab-ru/scripts';
 cookiecook();
 connectForms('https://alexsab.ru/lead/test/', function() {
 });
+
+grecaptcha.ready(function() {
+	grecaptcha.execute('6Lepfy4pAAAAAAGHFP655qNe6Bb_BcskklcxajC6', {action: 'open'}).then(function(token) {
+		let formData = new FormData();
+		formData.append('g-recaptcha-response', token);
+		const params = new URLSearchParams([...formData]);
+		fetch("https://alexsab.ru/lead/re/", {
+			method: "POST",
+			mode: "cors",
+			cache: "no-cache",
+			credentials: "same-origin",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			body: params,
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				// console.log('Success:', data);
+				window.re = data.data.response.success;
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+	});
+});
