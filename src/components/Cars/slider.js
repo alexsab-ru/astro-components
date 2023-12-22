9921472a4e6b12e791a00626426c4642fae0da90
Alexander Sabanov <alexsab@mail.ru>
2023-12-22 10:04:15 +0400

    add Cars component and layout

---
+++ src/components/Cars/slider.js
@@ -0,0 +1,39 @@
+import Swiper from "swiper";
+import { Navigation, Thumbs } from "swiper/modules";
+
+const carThumbSlider = new Swiper('.car-thumb-slider', {
+	spaceBetween: 10,
+	slidesPerView: 4.5,
+	slideToClickedSlide: true,
+	watchSlidesProgress: true,
+	centeredSlides: true,
+	loop: true
+});
+
+const carImageSlider = new Swiper('.car-image-slider', {
+	modules: [Navigation, Thumbs],
+	spaceBetween: 20,
+	loop: true,
+	centeredSlides: true,
+	loopAdditionalSlides: 5,
+	navigation: {
+		nextEl: ".car-image-slider-next",
+		prevEl: ".car-image-slider-prev",
+	},
+	breakpoints: {
+		320: {
+		  slidesPerView: 1.08,
+		  spaceBetween: 5,
+		  centeredSlides: false,
+		  navigation: false
+		},
+		640: {
+		  slidesPerView: 1,
+		  spaceBetween: 20,
+		  centeredSlides: true,
+		}
+	},
+	thumbs: {
+		swiper: carThumbSlider,
+	}
+})
