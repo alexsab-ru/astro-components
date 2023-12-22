c9dbf5e891d280a6bc55c51f80087e9c7da3bfb2
Alexander Sabanov <alexsab@mail.ru>
2023-12-22 10:04:54 +0400

    add alpine sorting

--- src/js/data.alpine.js
+++ src/js/data.alpine.js
@@ -69,6 +69,63 @@ document.addEventListener('alpine:init', () => {
 			});
 		}
 	}));
+	Alpine.data("sorting", () => ({
+		open: false,
+		carItems: document.querySelectorAll(".car-item"),
+		carListWrapper: document.querySelector(".car-list"), //wrapper
+		cars: [],
+		options: [
+			{ id: "default", title: "По умолчанию" },
+			{ id: "price_up", title: "По возрастанию цены" },
+			{ id: "price_down", title: "По убыванию цены" },
+			{ id: "asc", title: "По моделям" },
+		],
+		current: "default",
+		value: "",
+		setTitle() {
+			this.options.find((c) => {
+				if (c.id === this.current) {
+					this.value = c.title;
+				}
+			});
+		},
+		sortBy(id) {
+			this.current = id;
+			this.setTitle();
+			this.open = false;
+            if(id != 'default'){
+                this.cars.sort(function (a, b) {
+                    var priceA = parseFloat(a.getAttribute("data-price"));
+                    var priceB = parseFloat(b.getAttribute("data-price"));
+                    var modelA = a.getAttribute('data-model').toLowerCase();
+                    var modelB = b.getAttribute('data-model').toLowerCase();
+                    if(id === "price_up"){
+                        return priceA - priceB; //увелечение
+                    }else if(id == "price_down"){
+                        return priceB - priceA; //уменьшение
+                    }else if(id === 'asc'){
+                        if (modelA < modelB) {
+                            return -1;
+                        }
+                    }
+                })
+            }else{
+                this.cars = Array.from(this.carItems);
+            }
+
+			while (this.carListWrapper.firstChild) {
+				this.carListWrapper.removeChild(this.carListWrapper.firstChild);
+			}
+			this.cars.forEach(function (element) {
+				document.querySelector(".car-list").appendChild(element);
+			});
+		},
+		init() {
+			this.cars = Array.from(this.carItems);
+			this.setTitle();
+            // this.sortBy(this.current)
+		},
+	}));
 });
 
 // window.Alpine = Alpine;
