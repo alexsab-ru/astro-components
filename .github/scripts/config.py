a376efd2af11d8d3a75ef76919c5d81a54b16ac3
Alexander Sabanov <alexsab@mail.ru>
2024-02-15 12:59:32 +0400

    moved the config to an external file

---
+++ .github/scripts/config.py
@@ -0,0 +1,104 @@
+dealer = {
+    "city": "Оренбург",
+    "where": "Оренбурге",
+}
+# Словарь соответствия цветов
+model_mapping = {
+    "Atlas Pro": {
+        "folder": "Atlas Pro",
+        "color": {
+            "Черный": "black-metallic",
+            "Черный/черный": "black-metallic",
+            "Серый": "gray-metallic",
+            "Красный": "red-metallic",
+            "Серебристый": "silver-metallic",
+        }},
+    "Atlas, I": {
+        "folder": "Atlas Pro",
+        "color": {
+
+        }},
+    "Atlas, II": {
+        "folder": "Atlas-2024",
+        "color": {
+            "Черный": "black-metallic",
+            "Черный/черный": "black-metallic",
+            "Серебристый": "silver-metallic",
+            "Серый": "starry-blue-metallic",
+            "Белый": "white",
+        }},
+    "Coolray, I Рестайлинг": {
+        "folder": "New Coolray",
+        "color": {
+            "Красный": "bright-vermilion",
+            "Белый": "crystal-white",
+            "Синий": "cyber-blue",
+            "Серый": "magnetic-grey",
+            "Серебристый": "unicorn-grey",
+        }},
+    "Coolray, I": {
+        "folder": "Coolray",
+        "color": {
+            "Синий": "blue-metallic",
+            "Серый": "grey",
+            "Красный": "red",
+            "Серебристый": "silver-metallic",
+            "Белый": "white",
+        }},
+    "Emgrand L": {
+        "folder": "Emgrand",
+        "color": {
+            "Черный": "black-metallic",
+            "Синий": "blue-metallic",
+            "Золотой": "gold-metallic",
+            "Серый": "gray-metallic",
+            "Белый": "white-metallic",
+        }},
+    "Emgrand, II": {
+        "folder": "Emgrand",
+        "color": {
+            "Черный": "black-metallic",
+            "Синий": "blue-metallic",
+            "Золотой": "gold-metallic",
+            "Оранжевый": "gold-metallic",
+            "Серый": "gray-metallic",
+            "Белый": "white-metallic",
+        }},
+    "Monjaro": {
+        "folder": "Monjaro",
+        "color": {
+            "Черный": "black-metallic",
+            "Черный/черный": "black-metallic",
+            "Зеленый": "emerald-metallic",
+            "Синий": "emerald-metallic",
+            "Серый": "gray-metallic",
+            "Серебристый": "silver-metallic",
+            "Белый": "white-metallic",
+        }},
+    "Okavango": {
+        "folder": "Okavango",
+        "color": {
+
+        }},
+    "Tugella, I Рестайлинг": {
+        "folder": "Tugella",
+        "color": {
+            "Черный": "black-metallic",
+            "Черный/черный": "black-metallic",
+            "Серо-голубой": "gray-blue-metallic",
+            "Серый": "gray-metallic",
+            "Белый": "white-metallic",
+        }},
+    "X50": {
+        "folder": "Belgee X50",
+        "color": {
+            "Черный": "black",
+            "Черный/черный": "black",
+            "Серый": "gray",
+            "Красный": "red",
+            "Синий": "blue",
+            "Серебристый": "silver",
+            "Белый": "white",
+        }},
+    # ... добавьте другие модели по мере необходимости
+}
