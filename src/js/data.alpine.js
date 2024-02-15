8a5425dc8d9d878cead2683c371ef5a884f6bd63
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-02-15 09:24:40 +0500

    added filtering to models

--- src/js/data.alpine.js
+++ src/js/data.alpine.js
@@ -1,5 +1,7 @@
 import Alpine from 'alpinejs';
 
+import { declOfNums } from "@/js/utils/numbers.format";
+
 document.addEventListener('alpine:init', () => {
 	Alpine.data('header', () => ({
 		open: false,
@@ -81,13 +83,16 @@ document.addEventListener('alpine:init', () => {
 		carListWrapper: document.querySelector(".car-list"), //wrapper
 		cars: [],
 		options: [
-			{ id: "default", title: "По умолчанию" },
+			// { id: "default", title: "По умолчанию" },
 			{ id: "price_up", title: "По возрастанию цены" },
 			{ id: "price_down", title: "По убыванию цены" },
-			{ id: "asc", title: "По моделям" },
+			// { id: "asc", title: "По моделям" },
 		],
-		current: "default",
+		current: "price_up",
+		currentModel: "all",
 		value: "",
+		total: 0,
+		declOfNums,
 		setTitle() {
 			this.options.find((c) => {
 				if (c.id === this.current) {
@@ -99,26 +104,25 @@ document.addEventListener('alpine:init', () => {
 			this.current = id;
 			this.setTitle();
 			this.open = false;
-            if(id != 'default'){
-                this.cars.sort(function (a, b) {
-                    var priceA = parseFloat(a.getAttribute("data-price"));
-                    var priceB = parseFloat(b.getAttribute("data-price"));
-                    var modelA = a.getAttribute('data-model').toLowerCase();
-                    var modelB = b.getAttribute('data-model').toLowerCase();
-                    if(id === "price_up"){
-                        return priceA - priceB; //увелечение
-                    }else if(id == "price_down"){
-                        return priceB - priceA; //уменьшение
-                    }else if(id === 'asc'){
-                        if (modelA < modelB) {
-                            return -1;
-                        }
-                    }
-                })
-            }else{
-                this.cars = Array.from(this.carItems);
-            }
-
+			if(id != 'default'){
+				this.cars.sort(function (a, b) {
+					var priceA = parseFloat(a.getAttribute("data-price"));
+					var priceB = parseFloat(b.getAttribute("data-price"));
+					var modelA = a.getAttribute('data-model').toLowerCase();
+					var modelB = b.getAttribute('data-model').toLowerCase();
+					if(id === "price_up"){
+						return priceA - priceB; //увелечение
+					}else if(id == "price_down"){
+						return priceB - priceA; //уменьшение
+					}else if(id === 'asc'){
+						if (modelA < modelB) {
+							return -1;
+						}
+					}
+				})
+			}else{
+				this.cars = Array.from(this.carItems);
+			}
 			while (this.carListWrapper.firstChild) {
 				this.carListWrapper.removeChild(this.carListWrapper.firstChild);
 			}
@@ -126,10 +130,44 @@ document.addEventListener('alpine:init', () => {
 				document.querySelector(".car-list").appendChild(element);
 			});
 		},
+		addQueryParam(key, value) {
+			const url = new URL(window.location.href);
+			url.searchParams.set(key, value);
+			window.history.pushState({path:url.href}, '', url.href);
+		},
+		filteredCars(model){
+			this.total = 0;
+			const vm = this;
+			this.currentModel = model;
+			this.cars.forEach(function (element) {
+				element.style.display = 'none';
+				if(model){
+					if(element.dataset.model.toLowerCase() == model){
+						element.style.display = 'block';
+						vm.total = vm.total+Number(element.dataset.total)
+					}else if(!model || model == 'all'){
+						element.style.display = 'block';
+						vm.total = vm.total+Number(element.dataset.total)
+					}				
+				}
+			});
+			this.addQueryParam('model', model);
+		},
 		init() {
+			const vm = this;
 			this.cars = Array.from(this.carItems);
 			this.setTitle();
-            // this.sortBy(this.current)
+			this.sortBy(this.current)
+			const params = new URLSearchParams(document.location.search);
+			const modelParams = params.get('model');
+			console.log(modelParams);
+			if(modelParams){
+				this.filteredCars(modelParams);
+			}else{
+				this.cars.reduce((acc, val) => {
+					return vm.total = acc+Number(val.dataset.total);
+				}, 0)
+			}
 		},
 	}));
 });
