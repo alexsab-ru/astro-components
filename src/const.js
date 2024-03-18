f952d7cc19916abe00af93d90e86c521254e764b
Alexander Sabanov <alexsab@mail.ru>
2024-03-18 10:19:28 +0400

    add default PHONE in const

--- src/const.js
+++ src/const.js
@@ -1,11 +1,14 @@
-export const BASE_URL = 'https://alexsab-ru.github.io/astro-json/'; // Название сайта по умолчанию
-export const SITE_URL = 'localhost'; // Название сайта по умолчанию
+import { phoneFormat } from '@/js/utils/numbers.format';
+
+export const BASE_URL = 'https://alexsab-ru.github.io/astro-json/'; // Адрес для JSON
+export const SITE_URL = 'localhost'; // URL сайта по умолчанию
 export const SITE_NAME = 'Название сайта'; // Название сайта по умолчанию
 export const LEGAL_ENTITY = 'ООО «Юридическое название»'; // Юр лицо
 export const LEGAL_INN = '1234567890'; // ИНН
 export const LEGAL_CITY = 'г. Город'; // Город
 export const LEGAL_CITY_WHERE = 'Городе'; // где? в Городе
 export const SITE_DESCR = 'Официальный дилерский центр'; // Описание сайта по умолчанию
+export const PHONE = '+7 (999) 000-00-00'; // Телефон по умолчанию
 
 export const BRAND = 'Baic';
 
@@ -22,4 +25,4 @@ export const LINKS_MENU = [
 export const HEADER_TOP_LINE = '';
 
 export const AGREE_LABEL = '<span>Я согласен на</span><a href="/privacy-policy" class="underline transition-all hover:no-underline" target="_blank">обработку персональных данных</a>';
-export const FOOTER_INFO = '<sup>*</sup> Вся представленная на сайте информация, касающаяся автомобилей и сервисного обслуживания, носит информационный характер и не является публичной офертой, определяемой положениями ст. 437 ГК РФ. Все цены, указанные на данном сайте, носят информационный характер. Для получения подробной информации просьба обращаться к менеджерам отдела продаж по номеру телефона <a href="tel:+79991234567">+7&nbsp;(999) 123-45-67</a>. Опубликованная на данном сайте информация может быть изменена в любое время без предварительного уведомления.';
+export const FOOTER_INFO = '<sup>*</sup> Вся представленная на сайте информация, касающаяся автомобилей и сервисного обслуживания, носит информационный характер и не является публичной офертой, определяемой положениями ст. 437 ГК РФ. Все цены, указанные на данном сайте, носят информационный характер. Для получения подробной информации просьба обращаться к менеджерам отдела продаж по номеру телефона <a class="whitespace-nowrap" href="tel:' + phoneFormat(PHONE) + '">' + PHONE + '</a>. Опубликованная на данном сайте информация может быть изменена в любое время без предварительного уведомления.';
