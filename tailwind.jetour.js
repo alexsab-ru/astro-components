9a57fe2bb73ba5d139de4e80f63aeb3bb014f92f
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-03-28 15:05:37 +0500

    added social colors in tailwind

--- tailwind.jetour.js
+++ tailwind.jetour.js
@@ -19,6 +19,12 @@ export const theme = {
         400: '#FF9966',
         500: '#ff874a',
       }, // в основном используется 400 и 500
+      vk: '#4B76A8',
+      ok: '#F7931D',
+      fb: '#3C5A99',
+      youtube: '#FF0018',
+      tiktok: '#000000',
+      telegram: '#2FACE1',
     },
     screens: {
       'xs': {'max': '430px'},
