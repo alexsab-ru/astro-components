ffe993a0238a03e51d469ce8e4c973378f2b92c6
Alexander Sabanov <alexsab@mail.ru>
2024-04-26 14:27:16 +0400

    remove BASE_URL, SITE_URL const

--- src/const.js
+++ src/const.js
@@ -1,8 +1,4 @@
 import { phoneFormat } from '@/js/utils/numbers.format';
-// Адрес для JSON
-export const BASE_URL = 'https://alexsab-ru.github.io/astro-json/';
-// URL сайта по умолчанию
-export const SITE_URL = 'localhost';
 // Название сайта по умолчанию
 export const SITE_NAME = 'Название сайта';
 // Юр лицо
