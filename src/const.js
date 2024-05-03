06926d5694717bf1edcede9b0c360bd330a733f3
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-05-03 10:22:38 +0500

    add const LINK_WIDGET and LINK_WIDGET_ORGNIZATION to const.js

--- src/const.js
+++ src/const.js
@@ -20,6 +20,10 @@ export const BRAND = 'Baic';
 // Конечное время для таймера
 //string 2024-04-26 or 2024-04-26 23:59:59 or December 31 2015 or December 31 2015 23:59:59 GMT+02:00
 export const TIMER_ENDTIME = '';
+// Ссылка яндекс-виджета
+export const LINK_WIDGET = 'https://yandex.ru/map-widget/v1/-/';
+// Ссылка организации для виджета
+export const LINK_WIDGET_ORGNIZATION = '';
 // Ссылки под хедером
 export const LINKS_MENU = [
 	{url: 'cars/', name: 'Авто в наличии'},
