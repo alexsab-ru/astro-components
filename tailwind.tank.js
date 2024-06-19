6f3ef6a3dbaafd33ac6b78414d312adea273b779
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-06-19 09:27:00 +0500

    Create tailwind.tank.js

---
+++ tailwind.tank.js
@@ -0,0 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+const baseConfig = require('./tailwind.config');
+
+module.exports = {
+	...baseConfig,
+	theme: {
+		...baseConfig.theme,
+		extend: {
+			...baseConfig.theme.extend,
+			colors: {
+				...baseConfig.theme.extend.colors,
+				dark: '#252c2c',
+				accent: { // в основном в верстке используется 400 и 500
+					400: '#ff9549',
+					500: '#ff9549',
+				},
+			},
+			fontFamily: {
+				'sans': ['TT_TANK', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+}
