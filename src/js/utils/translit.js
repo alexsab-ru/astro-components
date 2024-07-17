2776b891583e8594f92d82bc57b20dbd058470a3
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-07-17 14:37:54 +0500

    Update translit.js

--- src/js/utils/translit.js
+++ src/js/utils/translit.js
@@ -1,25 +1,21 @@
 export function useTranslit(word) {
-	if (!word) {
-	  return false;
-	}
- 
+	if (!word) return false;
+
 	const translations = {
-	  bodytype: "Кузов",
-	  body_type: "Кузов",
-	  engine: "Объем двигателя",
-	  engine_volume: "Объем двигателя",
-	  gear_rus: "Трансмиссия",
-	  gear_box: "Коробка передач",
-	  engine_power: "Мощность",
-	  drive: "Привод",
-	  fuel: "Тип топлива",
-	  engine_type: "Тип топлива",
-	  year: "Год выпуска",
-	  color_rus: "Цвет кузова",
-	  color_simple: "Цвет кузова",
+		bodytype: "Кузов",
+		body_type: "Кузов",
+		engine: "Объем двигателя",
+		engine_volume: "Объем двигателя",
+		gear_rus: "Трансмиссия",
+		gear_box: "Коробка передач",
+		engine_power: "Мощность",
+		drive: "Привод",
+		fuel: "Тип топлива",
+		engine_type: "Тип топлива",
+		year: "Год выпуска",
+		color_rus: "Цвет кузова",
+		color_simple: "Цвет кузова",
 	};
- 
-	let rusVal = translations[word] || word;
- 
-	return rusVal;
+	
+	return translations[word] || word;
  }
