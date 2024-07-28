3767ec00d7eb9622939f1d4a32d59b6d65c6e5f1
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-07-28 10:11:49 +0500

    upd robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,8 +17,8 @@ export default defineConfig({
 			policy: [
 				{
 					userAgent: ["*"],
-					allow: ["/"],
-					disallow: ["/?*"],
+					// allow: ["/"],
+					disallow: ["*?*"],
 				},
 			  ],
 		}),
