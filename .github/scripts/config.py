6befda61124044745d3cc9ea5699641ec8dbaf83
Alexander Sabanov <alexsab@mail.ru>
2024-09-02 10:34:38 +0400

    upd cars colors

--- .github/scripts/config.py
+++ .github/scripts/config.py
@@ -4,23 +4,8 @@ dealer = {
 }
 # Словарь соответствия цветов
 model_mapping = {
-    "X90 PLUS": {
-        "folder": "X90 PLUS",
-        "color": {
-            "White": "c1b.png",
-            "Black": "c2b.png",
-            "Blue": "c3b.png",
-            "Violet": "c4b.png",
-            "Grey": "c5b.png",
-            "Белый": "c1b.png",
-            "Черный": "c2b.png",
-            "Синий": "c3b.png",
-            "Фиолетовый": "c4b.png",
-            "Серый": "c5b.png",
-        }
-    },
     "Dashing": {
-        "folder": "Dashing",
+        "folder": "dashing",
         "color": {
             "White": "c1b.png",
             "Black": "c2b.png",
@@ -38,28 +23,61 @@ model_mapping = {
             "Светло-серый": "c7b.png",
         }
     },
+    "T2": {
+        "folder": "t2",
+        "color": {
+            "Black": "c1b.png",
+            "Черный": "c1b.png",
+            "Lightgray": "c2b.png",
+            "Серебряный": "c2b.png",
+            "Светло-серый": "c2b.png",
+            "Золотой": "c3b.png",
+            "Бежевый": "c3b.png",
+            "Gold": "c3b.png",
+            "Brown": "c3b.png",
+            "Gray": "c4b.png",
+            "Grey": "c4b.png",
+            "Серый": "c4b.png",
+        }
+    },
+    "X50": {
+        "folder": "x50",
+        "color": {
+            "Blur": "blue.png",
+            "Синий": "blue.png",
+            "Black": "c1b.png",
+            "Черный": "c1b.png",
+            "Silver": "c2b.png",
+            "Серебряный": "c2b.png",
+            "Brown": "c3b.png",
+            "Коричневый": "c3b.png",
+            "Gray": "c4b.png",
+            "Серый": "c4b.png",
+        }
+    },
     "X70 PLUS": {
-        "folder": "X70 PLUS",
+        "folder": "x70plus",
         "color": {
             "White": "c1b.png",
-            "Grey": "c2b.png",
-            "Red": "c3b.png",
-            "Gray": "c4b.png",
-            "Blue": "c5b.png",
-            "Darkblue": "c6b.png",
-            "Black": "c7b.png",
             "Белый": "c1b.png",
+            "Grey": "c2b.png",
+            "Silver": "c2b.png",
             "Серый": "c2b.png",
+            "Red": "c3b.png",
             "Красный": "c3b.png",
+            "Gray": "c4b.png",
             "Серый": "c4b.png",
+            "Blue": "c5b.png",
             "Синий": "c5b.png",
+            "Darkblue": "c6b.png",
             "Темно-синий": "c6b.png",
+            "Black": "c7b.png",
             "Черный": "c7b.png",
             "Зеленый": "green.png",
         }
     },
     "X70": {
-        "folder": "X70",
+        "folder": "x70plus",
         "color": {
             "White": "c1b.png",
             "Белый": "c1b.png",
@@ -78,21 +96,19 @@ model_mapping = {
             "Черный": "c7b.png",
         }
     },
-    "T2": {
-        "folder": "X70",
+    "X90 PLUS": {
+        "folder": "x90plus",
         "color": {
-            "Black": "c1b.png",
-            "Черный": "c1b.png",
-            "Lightgray": "c2b.png",
-            "Серебряный": "c2b.png",
-            "Светло-серый": "c2b.png",
-            "Золотой": "c3b.png",
-            "Бежевый": "c3b.png",
-            "Gold": "c3b.png",
-            "Brown": "c3b.png",
-            "Gray": "c4b.png",
-            "Grey": "c4b.png",
-            "Серый": "c4b.png",
+            "White": "c1b.png",
+            "Black": "c2b.png",
+            "Blue": "c3b.png",
+            "Violet": "c4b.png",
+            "Grey": "c5b.png",
+            "Белый": "c1b.png",
+            "Черный": "c2b.png",
+            "Синий": "c3b.png",
+            "Фиолетовый": "c4b.png",
+            "Серый": "c5b.png",
         }
     }
     # ... добавьте другие модели по мере необходимости
