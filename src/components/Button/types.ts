a43e403cca88482d06c27ef258366f668b607e43
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-09-11 09:09:05 +0500

    Create types.ts for button component

---
+++ src/components/Button/types.ts
@@ -0,0 +1,12 @@
+export type TButtonData = {
+	view?: string,
+	classes?: string,
+	url?: string,
+	title?: string,
+	dataTitle?: string,
+	dataFormName?: string,
+	dataImg?: string,
+	dataImgPosition?: string,
+	icon?: string,
+	iconClasses?: string,
+}
