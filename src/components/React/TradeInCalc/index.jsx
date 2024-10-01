ec19ee510c380735fb6b9999a7520b5c632fd72b
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-10-01 14:40:03 +0500

    Update index.jsx

--- src/components/React/TradeInCalc/index.jsx
+++ src/components/React/TradeInCalc/index.jsx
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, StrictMode } from 'react';
 import VinForm from './VinForm';
 import StepPanel from './StepPanel';
+import { useCarInfo } from '@/store/useCarInfo';
+import {localBrands} from '@/store/local.brands'
+import axios from 'axios';
+import Loader from '../Loader/Loader';
+axios.defaults.headers.common['Authorization'] = `Basic ${import.meta.env.PUBLIC_MAXPOSTER_TOKEN}`;
 
 export default function TradeInCalc() {
-	const [step, setStep] = useState(0);
+	const {step, avtoInfo, brands, setBrands} = useCarInfo();	
+	useEffect(() => {
+		if (!brands.length) {
+		  axios
+			 .get('http://127.0.0.1:8321/api/trade-in/dynamic-directories/vehicle-brands')
+			 .then((res) => {
+				setBrands(res.data.data.vehicleBrands.filter( brand => {
+					const b = localBrands.find(b => b.name == brand.name)
+					if(!b){
+						return;
+					}
+					brand.popular = b.popular;
+					return brand;
+				}));
+			});
+		}
+	 }, [brands]);
 	return ( 
-		<div>
-			<StepPanel step={step} />
-			<VinForm />
-		</div> 
+		<>
+			<StrictMode>
+				<Loader />
+				<StepPanel />
+				{step === 0 && <VinForm /> }
+			</StrictMode>
+		</> 
 	);
 }
