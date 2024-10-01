4b52473832fbc949b45f205529943dfbe47f3890
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-10-01 14:39:34 +0500

    Create local.brands.js

---
+++ src/store/local.brands.js
@@ -0,0 +1,87 @@
+export const localBrands = [
+	{ name: "Acura", popular: true },
+	{ name: "Alfa Romeo", popular: true },
+	{ name: "Aston Martin", popular: false },
+	{ name: "Audi", popular: true },
+
+	{ name: "Bentley", popular: false },
+	{ name: "BMW", popular: true },
+	{ name: "Bugatti", popular: false },
+
+	{ name: "Cadillac", popular: true },
+	{ name: "Chery", popular: true },
+	{ name: "Chevrolet", popular: true },
+	{ name: "Chrysler", popular: true },
+	{ name: "Citroen", popular: true },
+
+	{ name: "Daewoo", popular: true },
+	{ name: "Datsun", popular: true },
+	{ name: "Dodge", popular: true },
+	{ name: "DS", popular: true },
+
+	{ name: "FAW", popular: true },
+	{ name: "Ferrari", popular: false },
+	{ name: "Fiat", popular: true },
+	{ name: "Ford", popular: true },
+
+	{ name: "Geely", popular: true },
+	{ name: "Genesis", popular: true },
+	{ name: "Geo", popular: false },
+	{ name: "GMC", popular: true },
+	{ name: "Great Wall", popular: true },
+
+	{ name: "Haval", popular: true },
+	{ name: "Honda", popular: true },
+	{ name: "Hummer", popular: true },
+	{ name: "Hyundai", popular: true },
+
+	{ name: "Infiniti", popular: true },
+	{ name: "International", popular: false },
+
+	{ name: "Jaguar", popular: false },
+	{ name: "Jeep", popular: true },
+
+	{ name: "Kia", popular: true },
+
+	{ name: "LADA (ВАЗ)", popular: true },
+	{ name: "Lamborghini", popular: true },
+	{ name: "Land Rover", popular: false },
+	{ name: "Landwind", popular: false },
+	{ name: "Lexus", popular: true },
+	{ name: "Lifan", popular: true },
+	{ name: "Lincoln", popular: false },
+	{ name: "Lotus", popular: false },
+
+	{ name: "Maserati", popular: false },
+	{ name: "Maybach", popular: false },
+	{ name: "Mazda", popular: true },
+	{ name: "Mercedes-Benz", popular: true },
+	{ name: "MINI", popular: true },
+	{ name: "Mitsubishi", popular: true },
+
+	{ name: "Nissan", popular: true },
+
+	{ name: "Opel", popular: true },
+
+	{ name: "Peugeot", popular: true },
+	{ name: "Pontiac", popular: false },
+	{ name: "Porsche", popular: false },
+
+	{ name: "Ravon", popular: false },
+	{ name: "Renault", popular: true },
+	{ name: "Rolls-Royce", popular: false },
+	{ name: "Rover", popular: false },
+
+	{ name: "Saab", popular: false },
+	{ name: "Seat", popular: false },
+	{ name: "Skoda", popular: true },
+	{ name: "Smart", popular: false },
+	{ name: "SsangYong", popular: true },
+	{ name: "Subaru", popular: true },
+	{ name: "Suzuki", popular: true },
+
+	{ name: "Toyota", popular: true },
+
+	{ name: "Volkswagen", popular: true },
+	{ name: "Volvo", popular: true },
+]
