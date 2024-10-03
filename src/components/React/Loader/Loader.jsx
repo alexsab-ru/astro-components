044836db7a32482a4aadd265f181bedfec807e54
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-10-03 10:34:46 +0500

    upd Loader class -> className

--- src/components/React/Loader/Loader.jsx
+++ src/components/React/Loader/Loader.jsx
@@ -1,8 +1,8 @@
 import './styles.scss';
 function Loader() {
 	return (
-		<div class="fixed h-full w-full top-0 left-0 bg-white/50 flex items-center justify-center z-[1000]">
-			<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+		<div className="fixed h-full w-full top-0 left-0 bg-white/50 flex items-center justify-center z-[1000]">
+			<div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
 		</div>
 	);
 }
