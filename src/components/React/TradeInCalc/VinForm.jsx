e0427498f84ab719bb95d78b4e187f247b02a797
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-10-03 10:35:40 +0500

    Update VinForm.jsx

--- src/components/React/TradeInCalc/VinForm.jsx
+++ src/components/React/TradeInCalc/VinForm.jsx
@@ -91,41 +91,46 @@ const VinForm = () => {
 		setBodyNumber('');
 	}, [vin]);
 	return (
-		<form className="mb-10 vue-form" onSubmit={handleSubmit(onSubmit)}>
-			<h3 className="text-2xl font-bold mb-6">Введите VIN-номер</h3>
-			<div className="flex">
-				<input
-					type="text"
-					className={`w-[270px] border-2 transition-all focus:border-accent-500 px-4 py-2.5 outline-none text-black border-gray-100 ${vin ? '!border-black !text-black' : ''}`}
-					placeholder="Введите VIN-номер"
-					value={vin}
-					{...register('vin')}
-					onChange={(e) => setVIN(e.target.value)}
-				/>
-				<button
-					className="bg-black text-white w-[48px] h-[48px] m-0 flex justify-center items-center transition-opacity hover:bg-accent-400 disabled:opacity-90 disabled:cursor-not-allowed"
-					disabled={isSubmitting}
-				>
-					<svg
-						width="20"
-						height="20"
-						viewBox="0 0 20 20"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-						preserveAspectRatio="xMidYMid"
-						className=""
-					>
-						<path
-							data-v-134dc84b=""
-							d="M8.5 14l4-4-4-4"
-							stroke="currentColor"
-							strokeWidth="2"
-						></path>
-					</svg>
-				</button>
+		<>
+			<div className="mb-6">
+				<p>Заполните данные о&nbsp;вашем автомобиле и&nbsp;получите предварительную оценку. <br className="hidden md:block" />Вы&nbsp;можете сразу отправить ваш расчет официальному дилеру Kia и&nbsp;уточнить <br className="hidden md:block" />детали выкупа автомобиля.</p>
 			</div>
-			{errors.vin && <ShowErrors errors={errors.vin.message} /> }
-		</form>
+			<form className="mb-10 vue-form" onSubmit={handleSubmit(onSubmit)}>
+				<h3 className="text-2xl font-bold mb-6">Введите VIN-номер</h3>
+				<div className="flex">
+					<input
+						type="text"
+						className={`w-[270px] border-2 transition-all focus:border-accent-500 px-4 py-2.5 outline-none text-black border-gray-100 ${vin ? '!border-black !text-black' : ''}`}
+						placeholder="Введите VIN-номер"
+						value={vin}
+						{...register('vin')}
+						onChange={(e) => setVIN(e.target.value)}
+					/>
+					<button
+						className="bg-black text-white w-[48px] h-[48px] m-0 flex justify-center items-center transition-opacity hover:bg-accent-400 disabled:opacity-90 disabled:cursor-not-allowed"
+						disabled={isSubmitting}
+					>
+						<svg
+							width="20"
+							height="20"
+							viewBox="0 0 20 20"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							preserveAspectRatio="xMidYMid"
+							className=""
+						>
+							<path
+								data-v-134dc84b=""
+								d="M8.5 14l4-4-4-4"
+								stroke="currentColor"
+								strokeWidth="2"
+							></path>
+						</svg>
+					</button>
+				</div>
+				{errors.vin && <ShowErrors errors={errors.vin.message} /> }
+			</form>
+		</>
 	);
 }
 
