fc58867e08a53a8fa091579d250f5428bf8c2d81
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-10-09 17:04:56 +0500

    create fetchCarsInfo action

--- src/store/useCarInfo.tsx
+++ src/store/useCarInfo.tsx
@@ -2,6 +2,14 @@ import axios from 'axios';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
+import { localBrands } from '@/store/local.brands'
+
+type TBrand = {
+	id: number; 
+	name: string; 
+	popular?: boolean;
+}
+
 export type TCarState = {
 	loading: boolean;
 	vinState: string;
@@ -10,7 +18,11 @@ export type TCarState = {
 	error: string;
 	noResultFetchMessage: string;
 	avtoInfo: any;
-	brands: any;
+	brands: TBrand[];
+	models: any;
+	years: any;
+	generations: any;
+	bodyConfigurations: any;
 }
 
 export type TCarInfoActions = {
@@ -21,14 +33,19 @@ export type TCarInfoActions = {
 	clearError: () => void;
 	setMessage: (mes: string) => void;
 	recalculate: () => void;
-	setBodyNumber: (vin2: string) => void;
+	setBodyNumber: (vin: string) => void;
 	setAvtoInfo: (data: any) => void;
 	setBrands: (data: any) => void;
+	setModels: (data: any) => void;
+	setYears: (data: any) => void;
+	setGenerations: (data: any) => void;
+	setBodyConfigurations: (data: any) => void;
 	showLoader: () => void;
 	hideLoader: () => void;
+	fetchCarsInfo: (data: any) => void;
 }
 
-export const useCarInfo = create<TCarState & TCarInfoActions>()(devtools((set) => ({
+export const useCarInfo = create<TCarState & TCarInfoActions>((set, get) => ({
 	loading: true,
 	vinState: "",
 	bodyNumber: "",
@@ -37,6 +54,10 @@ export const useCarInfo = create<TCarState & TCarInfoActions>()(devtools((set) =
 	noResultFetchMessage: "Ничего не выбрано",
 	avtoInfo: {},
 	brands: [],
+	models: [],
+	years: [],
+	generations: [],
+	bodyConfigurations: [],
 	setVIN: (vin) => set({ vinState: vin }),
 	setError: () => set({ error: 'Извините, сервис временно недоступен. Мы уже работает над этим, повторите попыку позже.' }),
 	clearError: () => set({ error: '' }),
@@ -47,6 +68,82 @@ export const useCarInfo = create<TCarState & TCarInfoActions>()(devtools((set) =
 	setBodyNumber: (vin) => set({ bodyNumber: vin }),
 	setAvtoInfo: (data) => set({ avtoInfo: data }),
 	setBrands: (data) => set({ brands: data }),
+	setModels: (data) => set({ models: data }),
+	setYears: (data) => set({ years: data }),
+	setGenerations: (data) => set({ generations: data }),
+	setBodyConfigurations: (data) => set({ bodyConfigurations: data }),
 	showLoader: () => set({ loading: true }),
 	hideLoader: () => set({ loading: false }),
-})))
+	fetchCarsInfo: async (data) => {
+		data.params.categoryId = 1; // 1 - легковые автомобили
+		get().setVIN('');
+		get().setBodyNumber('');
+		const url = new URL(data.url);
+		Object.keys(data.params).forEach(key => url.searchParams.append(key, data.params[key])) // добавляем GET параметры	
+		get().showLoader(); // показываем загрузку
+		try {
+			const response = await axios.get(url.href);
+			const fetchData = response.data; // получаем данные
+			if(fetchData.status === 'success'){
+				switch (data.name) {
+					case 'brands':
+						const filteredBrands = fetchData.data.vehicleBrands.filter((brand: TBrand) => {
+							const b = localBrands.find(b => b.name.toLowerCase() === brand.name.toLowerCase());
+							if (!b) return false; // Убедитесь, что возвращаете true/false для корректной фильтрации
+							brand.popular = b.popular;
+							return true;
+						});
+						set({ brands: filteredBrands });
+						break;
+					case 'models':
+						set({ years: [] });
+						set({ generations: [] });
+						set({ bodyConfigurations: [] });
+						const currentBrand = get().brands.find(b => b.id === Number(data.params.brandId));
+						if(!currentBrand){ get().setError() }			
+						get().setAvtoInfo({ brand: currentBrand });
+						set({ models: fetchData.data.vehicleModels });
+						break;
+					case 'years':
+						set({ generations: [] });
+						set({ bodyConfigurations: [] });
+						const currentModel = get().models.find(m => m.id === Number(data.params.modelId));
+						if(!currentModel){ get().setError() }
+						delete currentModel.category;			
+						get().setAvtoInfo({ ...get().avtoInfo, model: currentModel });
+						set({ years: fetchData.data.vehicleYears });
+						break;
+					case 'generations':
+						set({ bodyConfigurations: [] });
+						if(!data.params.year){ get().setError() }
+						get().setAvtoInfo({ ...get().avtoInfo, year: data.params.year });
+						let generations = fetchData.data.vehicleGenerations;
+						if(generations.length == 1){
+							generations[0].name = "Без поколения";
+						}
+						set({ generations: generations });
+						break;
+					case 'bodyConf':
+						const currentGeneration = get().generations.find(g => g.id === Number(data.params.generationId));
+						if(!currentGeneration){ get().setError() }		
+						get().setAvtoInfo({ ...get().avtoInfo, generation: currentGeneration });
+						set({ bodyConfigurations: fetchData.data.vehicleBodyConfigurations });
+						break;
+					case 'modifications':
+						// commit("setModifications", response.data.vehicleModifications);
+						break;
+					default:
+						break;
+				}
+			} else {
+				get().setError(); // если не удача, выводим ошибку
+				console.log("fetch-"+data.name, fetchData);
+			}	
+		} catch (error) {
+			get().setError(); // если не удача, выводим ошибку
+			console.error("fetch-"+data.name, error);
+		} finally {
+			get().hideLoader();
+		}
+	}
+}))
