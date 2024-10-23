e5add995d489faee80bc743cdf2ac6e6b0546ee0
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-10-23 18:10:50 +0500

    Update slider.js

--- src/components/Model/sections/slider.js
+++ src/components/Model/sections/slider.js
@@ -9,18 +9,32 @@ if(sliders.length){
 			modules: [Navigation, Pagination, Keyboard],
 			loop: false,
 			speed: 1000,
+			spaceBetween: 20,
 			keyboard: {
 				enabled: true,
 				onlyInViewport: true,
 			},
-			// pagination: {
-			// 	el: ".banner-pagination",
-			// 	clickable: true,
-			// },
 			navigation: {
 				nextEl: ".banner-button-next",
 				prevEl: ".banner-button-prev",
 			},
+			// pagination: {
+			// 	el: '.banner-pagination',
+			// 	type: 'bullets',
+			// 	clickable: true,
+			// },
+			breakpoints: {
+				// when window width is >= 320px
+				320: {
+				  slidesPerView: 1,
+				},
+				640: {
+				  slidesPerView: 2,
+				},
+				1024: {
+				  slidesPerView: 3,				  
+				}
+			 }
 		})
 	})
 }
