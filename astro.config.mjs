3b293e3ea0ef7a637b54f8aae9be2b31c37ffce2
Alexander Sabanov <alexsab@mail.ru>
2024-10-28 16:17:03 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,6 +32,6 @@ export default defineConfig({
 	vite: {
 		plugins: [yaml()]
 	},
-	site: 'https://alexsab-ru.github.io',
-	base: '/belgee-partner-orenburg.ru/'
+	site: 'belgee-partner-orenburg.ru',
+	base: '/'
 });
