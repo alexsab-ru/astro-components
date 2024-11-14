9fcb00599076087b3dcdcf8c2c96c1a8d89da3c3
Alexander Sabanov <alexsab@mail.ru>
2024-11-14 19:26:16 +0700

    Update config.py

--- .github/scripts/config.py
+++ .github/scripts/config.py
@@ -4,171 +4,11 @@ dealer = {
 }
 # Словарь соответствия цветов
 model_mapping = {
-    "001": {
-        "folder": "001",
-        "color": {
-            "Белый": "white.webp",
-        }
-    },
-    "Atlas Pro": {
-        "folder": "Atlas Pro",
-        "color": {
-            "Черный": "black-metallic.webp",
-            "Черный/черный": "black-metallic.webp",
-            "Серый": "gray-metallic.webp",
-            "Серый металлик": "gray-metallic.webp",
-            "Красный": "red-metallic.webp",
-            "Серебристый": "silver-metallic.webp",
-            "Белый": "white.webp",
-        }},
-    "Atlas, I": {
-        "folder": "Atlas Pro",
-        "color": {
-
-        }},
-    "Atlas, II": {
-        "folder": "Atlas-2024",
-        "color": {
-            "Черный": "black-metallic.webp",
-            "Черный/черный": "black-metallic.webp",
-            "Серебристый": "silver-metallic.webp",
-            "Серый": "silver-metallic.webp",
-            "Синий": "starry-blue-metallic.webp",
-            "Серо-голубой": "starry-blue-metallic.webp",
-            "Белый": "white.webp",
-            "Зелёный": "racing-green.webp",
-            "Зеленый": "racing-green.webp",
-        }},
-    "Coolray, I Рестайлинг": {
-        "folder": "New Coolray",
-        "color": {
-            "Красный": "bright-vermilion.webp",
-            "Белый": "crystal-white.webp",
-            "Синий": "cyber-blue.webp",
-            "Голубой": "cyber-blue.webp",
-            "Серый": "magnetic-grey.webp",
-            "Серый металлик": "magnetic-grey.webp",
-            "Серебристый": "unicorn-grey.webp",
-        }},
-    "Coolray, I": {
-        "folder": "Coolray",
-        "color": {
-            "Синий": "blue-metallic.webp",
-            "Серый": "grey.webp",
-            "Красный": "red.webp",
-            "Серебристый": "silver-metallic.webp",
-            "Белый": "white.webp",
-        }},
-    "Emgrand L": {
-        "folder": "Emgrand",
-        "color": {
-            "Черный": "black-metallic.webp",
-            "Синий": "blue-metallic.webp",
-            "Золотой": "gold-metallic.webp",
-            "Серый": "gray-metallic.webp",
-            "Белый": "white-metallic.webp",
-        }},
-    "Emgrand, II": {
-        "folder": "Emgrand",
-        "color": {
-            "Черный": "black-metallic.webp",
-            "Темно-синий": "blue-metallic.webp",
-            "Синий": "blue-metallic.webp",
-            "Желтый": "gold-metallic.webp",
-            "Золотой": "gold-metallic.webp",
-            "Бежевый": "gold-metallic.webp",
-            "Оранжевый": "gold-metallic.webp",
-            "Серый": "gray-metallic.webp",
-            "Серо-голубой": "gray-blue-metallic.webp",
-            "Белый": "white-metallic.webp",
-        }},
-    "Geometry G6": {
-        "folder": "Geometry G6",
-        "color": {
-            "Серый": "gray.webp",
-        }},
-    "Monjaro": {
-        "folder": "Monjaro",
-        "color": {
-            "Черный": "black-metallic.webp",
-            "Черный/черный": "black-metallic.webp",
-            "Зеленый": "emerald-metallic.webp",
-            "Синий": "emerald-metallic.webp",
-            "Серый": "gray-metallic.webp",
-            "Серый металлик": "gray-metallic.webp",
-            "Серебристый": "silver-metallic.webp",
-            "Белый": "white-metallic.webp",
-        }},
-    "Okavango": {
-        "folder": "Okavango",
-        "color": {
-            "Синий": "indigo-blue.webp",
-            "Синий металлик": "indigo-blue.webp",
-            "Черный/черный": "ink-black.webp",
-            "Черный": "ink-black.webp",
-            "Черный металик": "ink-black.webp",
-            "Серый металлик": "basalt-gray.webp",
-            "Серый": "basalt-gray.webp",
-            "Белый": "crystal-white.webp",
-        }},
-    "Okavango, I Рестайлинг": {
-        "folder": "Okavango",
-        "color": {
-            "Синий": "indigo-blue.webp",
-            "Синий металлик": "indigo-blue.webp",
-            "Черный/черный": "ink-black.webp",
-            "Черный": "ink-black.webp",
-            "Черный металик": "ink-black.webp",
-            "Серый металлик": "basalt-gray.webp",
-            "Серый": "basalt-gray.webp",
-            "Белый": "crystal-white.webp",
-        }},
-    "Tugella, I Рестайлинг": {
-        "folder": "Tugella",
-        "color": {
-            "Черный": "black-metallic.webp",
-            "Черный/черный": "black-metallic.webp",
-            "Серо-голубой": "gray-blue-metallic.webp",
-            "Серый": "gray-metallic.webp",
-            "Серый металлик": "gray-metallic.webp",
-            "Белый": "white-metallic.webp",
-        }},
-    "Preface, I Рестайлинг 2": {
-        "folder": "Preface",
-        "color": {
-            "Black": "black.webp",
-            "Черный": "black.webp",
-            "Gray-blue": "gray-blue.webp",
-            "Серо-голубой": "gray-blue.webp",
-            "Gray-purple": "gray-purple.webp",
-            "Серо-фиолетовый": "gray-purple.webp",
-            "Gray": "gray.webp",
-            "Серый": "gray.webp",
-            "Green": "green.webp",
-            "Зеленый": "green.webp",
-            "White": "white.webp",
-            "Белый": "white.webp",
-        }},
     "X50": {
-        "folder": "Belgee X50",
-        "color": {
-            "Черный": "black.webp",
-            "Черный/черный": "black.webp",
-            "Серый": "gray.webp",
-            "Серый металлик": "gray.webp",
-            "Красный": "red.webp",
-            "Синий": "blue.webp",
-            "Серебристый": "silver.webp",
-            "Белый": "white.webp",
-        }},
-    "X70": {
-        "folder": "Belgee X70",
+        "folder": "x50",
         "color": {
             "Black": "black.webp",
             "Черный": "black.webp",
-            "Черный/черный": "black.webp",
-            "Blue": "blue.webp",
-            "Синий": "blue.webp",
             "Gray": "gray.webp",
             "Серый": "gray.webp",
             "Red": "red.webp",
@@ -178,14 +18,19 @@ model_mapping = {
             "White": "white.webp",
             "Белый": "white.webp",
         }},
-    "Cityray": {
-        "folder": "Cityray",
+    "X70": {
+        "folder": "x70",
         "color": {
+            "Black": "black.webp",
+            "Черный": "black.webp",
+            "Blue": "blue.webp",
+            "Синий": "blue.webp",
             "Gray": "gray.webp",
             "Серый": "gray.webp",
-            "Blue": "light-blue.webp",
-            "Синий": "light-blue.webp",
-            "Голубой": "light-blue.webp",
+            "Red": "red.webp",
+            "Красный": "red.webp",
+            "Silver": "silver.webp",
+            "Серебристый": "silver.webp",
             "White": "white.webp",
             "Белый": "white.webp",
         }},
