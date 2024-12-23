7ea21db4de461b02eabb33ad956fc39b99e8b651
Alexander Sabanov <alexsab@mail.ru>
2024-12-23 20:59:44 +0700

    add catalog menu

--- src/const.js
+++ src/const.js
@@ -28,6 +28,7 @@ export const LINK_WIDGET_ORGNIZATION = '';
 // Ссылки под хедером
 export const LINKS_MENU = [
 	{url: 'cars/', name: 'Авто в наличии'},
+	// {url: 'catalog/', name: 'Каталог'},
 	// {url: 'used_cars/', name: 'Авто с пробегом'},
 	{url: 'models/', name: 'Модели'},
 	// {url: 'trade-in/', name: 'Оценка автомобиля'}
