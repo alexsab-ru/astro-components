df649c5cc5b8ba8b077771c6ab0d4e75bb60fd08
Alexander Sabanov <alexsab@mail.ru>
2025-01-23 18:22:33 +0700

    hide catalog

--- src/const.js
+++ src/const.js
@@ -28,7 +28,7 @@ export const LINK_WIDGET_ORGNIZATION = 'CDVxR0L4';
 // Ссылки под хедером
 export const LINKS_MENU = [
 	{url: 'cars/', name: 'Авто в наличии'},
-	{url: 'catalog/', name: 'Каталог'},
+	// {url: 'catalog/', name: 'Каталог'},
 	// {url: 'used_cars/', name: 'Авто с пробегом'},
 	{url: 'models/', name: 'Модели'},
 	// {url: 'trade-in/', name: 'Оценка автомобиля'},
