d273e194e9989cceddaa6a72a339fc056f0b6fc6
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-01-25 12:25:16 +0500

    refactoring and add filtered to publish date sortingAndFilteringPosts.js

--- src/js/utils/sortingAndFilteringPosts.js
+++ src/js/utils/sortingAndFilteringPosts.js
@@ -1,17 +1,17 @@
-const now = new Date();
-now.setHours(0, 0, 0, 0);
+const nowWithTime = new Date();
+const nowWithoutTime = new Date();
+nowWithoutTime.setHours(0, 0, 0, 0);
 
 export function sortingAndFilteringPosts(posts) {
 	return posts
-		.sort((a, b) => {
-			return a.data.pubDate && b.data.pubDate
-				? b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-				: 0;
-		})
-		.filter((post) => !post.data.draft)
-		.filter((post) => {
+		.sort((a, b) => a.data.pubDate && b.data.pubDate ? b.data.pubDate.valueOf() - a.data.pubDate.valueOf() : 0)
+		.filter(post => !post.data.draft)
+		.filter(post => {
 			if (post.data?.toDate && typeof post.data?.toDate === 'object') {
-				return now <= post.data?.toDate ? post : null;
+				return nowWithoutTime <= post.data?.toDate ? post : null;
+			}
+			if (post.data?.pubDate && typeof post.data?.pubDate === 'object') {
+				return nowWithTime < post.data?.pubDate ? null : post;
 			}
 			return post;
 		});
