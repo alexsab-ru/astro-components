ff1fd1340bb511d93d726d67d15d7a8a3d76da9f
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-02-25 13:19:04 +0500

    Create types.ts for banner

---
+++ src/components/Banner/types.ts
@@ -0,0 +1,30 @@
+type TBadge = {
+	autoname: string
+	title: string
+	descr: string
+	image: string
+	position: string
+	colorText: string
+	bg: boolean
+}
+export type TBanner = {
+	id?: number
+	show?: boolean
+	type?: string
+	view?: string
+	videoUrl?: string
+	imageUrl: string
+	mobileImageUrl?: string
+	imagePosition?: string
+	bannerUrl?: string
+	target?: string
+	title?: string
+	descr?: string
+	btn?: string
+	btnUrl?: string
+	btnColor?: string
+	dataTitle?: string
+	dataFormName?: string
+	badge?: TBadge
+	autoplay?: number
+}
