b0421d5ccc0143088866f506aeb87df2c9472287
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-04-09 17:35:31 +0500

    change &nbsp; on nowrap style

--- src/components/React/TradeInCalc/avto/InfoResult.jsx
+++ src/components/React/TradeInCalc/avto/InfoResult.jsx
@@ -4,12 +4,12 @@ function AvtoInfoResult() {
 	const {avtoInfo, recalculate, setStep, selfSale, dealerPrice } = useCarInfo();
 	return ( 
 		<>
-			<h3 className="text-3xl font-bold mb-10">Расчетная цена {avtoInfo?.brand.name} {avtoInfo?.model.name} — {avtoInfo?.year}&nbsp;г.в.</h3>
+			<h3 className="text-3xl font-bold mb-10">Расчетная цена {avtoInfo?.brand.name} {avtoInfo?.model.name} — <span className="text-nowrap">{avtoInfo?.year} г.в.</span></h3>
 			<div className="lg:w-1/3 bg-gray-100 p-6">
 				<h3 className="text-2xl font-bold mb-6">Выкуп</h3>
 				{dealerPrice !== 0 ? (
 					<div className="mb-6">
-						<h3 className="text-3xl font-bold">{ dealerPrice.toLocaleString('ru-RU') }&nbsp;₽</h3>
+						<h3 className="text-3xl font-bold text-nowrap">{ dealerPrice.toLocaleString('ru-RU') } ₽</h3>
 						<p className="!text-xs">Предварительная оценка стоимости автомобиля <sup>*</sup></p>
 					</div>
 				) : (
@@ -22,7 +22,7 @@ function AvtoInfoResult() {
 				<div>
 					<p className="!mb-3">При самостоятельной продаже</p>
 				{selfSale !== 0 ? (
-						<p className="font-bold !mb-2">{ selfSale.toLocaleString('ru-RU') }&nbsp;₽</p>
+						<p className="font-bold !mb-2 text-nowrap">{ selfSale.toLocaleString('ru-RU') } ₽</p>
 					) : (
 						<p className="!mb-0 !text-xs">К сожалению, по указанным параметрам сделать расчет не удалось</p>
 					)}
@@ -33,7 +33,7 @@ function AvtoInfoResult() {
 				><svg  width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" className="w-5"><path d="M16.391 6.5c-1.24-2.092-3.56-3.5-6.215-3.5C6.213 3 3 6.134 3 10s3.213 7 7.176 7S16.39 14.083 17 11.75" stroke="currentColor" strokeWidth={1.5}></path><path d="M17 2.5V7h-4.5" stroke="currentColor" strokeWidth={1.5}></path></svg>Пересчитать</button>
 			</div>
 
-			<p className="text-gray-600"><sup>*</sup> Указанная расчетная стоимость является приблизительной и&nbsp;действует при выкупе автомобиля с&nbsp;пробегом. Информация предоставлена <a href="https://maxposter.ru" target="_blank" className="">MaxPoster</a></p>
+			<p className="text-gray-600"><sup>*</sup> Указанная расчетная стоимость является приблизительной и действует при выкупе автомобиля с пробегом. Информация предоставлена <a href="https://maxposter.ru" target="_blank" className="">MaxPoster</a></p>
 
 		</>
 	);
