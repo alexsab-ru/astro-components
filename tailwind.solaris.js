33336b0134164327ea670870935b60ebdaf2176b
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-04-23 11:10:11 +0500

    Create tailwind.solaris.js

---
+++ tailwind.solaris.js
@@ -0,0 +1,33 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+const baseConfig = require('./tailwind.config');
+
+module.exports = {
+	...baseConfig,
+	theme: {
+		...baseConfig.theme,
+		extend: {
+			...baseConfig.theme.extend,
+			colors: {
+				...baseConfig.theme.extend.colors,
+				accent: {
+					400: "#418fde",
+					500: "#1b365d",
+				},
+				'brand-color': "#3a3c4e"
+			},
+			fontFamily: {
+				'sans': ['Mulish', ...defaultTheme.fontFamily.sans],
+			},
+			borderRadius: {
+				btn: '100px'
+			},
+			backgroundSize: {
+                'size-200': '150% 200%',
+            },
+            backgroundPosition: {
+                'pos-0': '0% 0%',
+                'pos-100': '100% 100%',
+            },
+		},
+	},
+}
