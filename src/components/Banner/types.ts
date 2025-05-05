0b62016c18f0f07ead455cc0f4940faea2471d89
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-05-05 12:32:04 +0500

    added new types - Viewport and TMediaKeys

--- src/components/Banner/types.ts
+++ src/components/Banner/types.ts
@@ -1,3 +1,4 @@
+export type Viewport = 'mobile' | 'tablet' | 'desktop';
 export type TBadge = {
 	autoname?: string
 	title?: string
@@ -7,14 +8,24 @@ export type TBadge = {
 	colorText?: string
 	bg?: boolean
 }
+export type TMediaKeys = {
+	desktop: string
+	tablet: string
+	mobile: string
+}
 export type TBanner = {
 	id?: number
 	show?: boolean
 	type?: string
 	view?: string
+
+	image?: TMediaKeys
+	video?: TMediaKeys
+	position?: TMediaKeys
+
 	videoUrl?: string
 	mobileVideoUrl?: string
-	imageUrl: string
+	imageUrl?: string
 	tabletImageUrl?: string
 	mobileImageUrl?: string
 	imagePosition?: string
