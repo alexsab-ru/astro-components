ca51e9a43d17110a841c2f0f974598549a251663
Oksana T. <pochtoviy-yashik@mail.ru>
2025-05-16 16:29:54 +0400

    hide news

--- src/const.js
+++ src/const.js
@@ -27,7 +27,7 @@ export const LINKS_MENU = [
 	},
 	// {url: 'trade-in/', name: 'Оценка автомобиля'},
 	{url: 'special-offers/', name: 'Спецпредложения'},
-	{url: 'news/', name: 'Новости'},
+	// {url: 'news/', name: 'Новости'},
 	{url: 'test-drive/', name: 'Запись на тест-драйв'},
 	{url: 'service-request/', name: 'Запись на сервис'},
 	{url: '#services', name: 'Услуги'},
@@ -37,7 +37,7 @@ export const LINKS_MENU = [
 // Коллекции
 export const COLLECTIONS = [
 	{name: 'special-offers', title: 'Спецпредложения'},
-	{name: 'news', title: 'Новости'},
+	{name: 'for-owners', title: 'Владельцам'},
 ];
 
 // Текст согласия в формах
