1a8854593dc95d74b7f76fdad67b75c91ba41c7e
Alexander Sabanov <alexsab@mail.ru>
2025-06-03 11:59:57 +0400

    add send_telegram_message to send simple message

--- .github/scripts/sh/send_telegram.sh
+++ .github/scripts/sh/send_telegram.sh
@@ -47,3 +47,42 @@ send_telegram_messages() {
         done
     done <<< "$chat_ids"
 }
+
+send_telegram_message() {
+    # Очищаем входные параметры от кавычек
+    local token=$(trim_quotes "$1")
+    local chat_ids=$(trim_quotes "$2")
+    local message=$(trim_quotes "$3")
+
+    if [ -z "$token" ] || [ -z "$chat_ids" ] || [ -z "$message" ]; then
+        echo "Error: Missing required parameters" >&2
+        echo "Usage: send_telegram_message token chat_ids message" >&2
+        return 1
+    fi
+
+    # Разбиваем chat_ids по переносу строки
+    while IFS= read -r chat_line; do
+        # Разбиваем строку по запятой
+        IFS=',' read -r chat_id message_thread_id <<< "$chat_line"
+        
+        echo "Sending message to Telegram chat $chat_id"
+        
+        # Формируем базовый URL запроса
+        local request_url="https://api.telegram.org/bot${token}/sendMessage"
+        local request_data="chat_id=${chat_id}&parse_mode=HTML&text=${message}&disable_web_page_preview=true"
+        
+        # Добавляем message_thread_id, если он есть
+        if [ ! -z "$message_thread_id" ]; then
+            request_data="${request_data}&message_thread_id=${message_thread_id}"
+        fi
+        
+        RESPONSE=$(curl -s -X POST "$request_url" -d "$request_data")
+
+        if ! echo "$RESPONSE" | grep -q '"ok":true'; then
+            echo "Error sending message to chat $chat_id: $RESPONSE" >&2
+        fi
+        
+        # Добавляем небольшую задержку между отправками сообщений
+        sleep 1
+    done <<< "$chat_ids"
+}
