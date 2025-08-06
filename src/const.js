e17fbe732cf99e2d0fc1a32d3289720c5a97edda
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-08-06 14:49:07 +0500

    new logic of menu formation

--- src/const.js
+++ src/const.js
@@ -13,27 +13,35 @@ import { groupArrayByKey } from '@/js/utils/groupArrayByKey';
 import modelsData from '@/data/models.json';
 const { models } = modelsData;
 const groupModelsByBrand = groupArrayByKey(models.filter(model => model.show), 'mark_id');
-const children = Object.keys(groupModelsByBrand).reduce((acc, key) => {
-	acc[key] = groupModelsByBrand[key].map(model => ( { url: `/models/${model.id}/`, name: `${model.name.toUpperCase()}`, thumb: model.thumb } ) );
-	return acc;
-}, {});
-export const LINKS_MENU = [
-	{url: '/cars/', name: 'Авто в наличии'},
-	// {url: 'catalog/', name: 'Каталог'},
-	// {url: 'used_cars/', name: 'Авто с пробегом'},
-	{ 
-		url: '/models/', 
-		name: 'Модели',
-		children
-	},
-	// {url: 'trade-in/', name: 'Оценка автомобиля'},
-	{url: '/special-offers/', name: 'Спецпредложения'},
-	{url: '/news/', name: 'Новости'},
-	{url: '/test-drive/', name: 'Запись на тест-драйв'},
-	{url: '/service-request/', name: 'Запись на сервис'},
-	{url: '/#services', name: 'Услуги'},
-	{url: '/contacts/', name: 'Контакты'},
-];
+
+const childrenGroup = {
+	models: Object.keys(groupModelsByBrand).reduce((acc, key) => {
+		acc[key] = groupModelsByBrand[key].map(model => ( { url: `/models/${model.id}/`, name: `${model.name.toUpperCase()}`, thumb: model.thumb } ) );
+		return acc;
+	}, {}),
+};
+
+let menu = [];
+
+try {
+	menu = await import('@/data/menu.json');	
+	menu = menu.default || menu; // Обработка случая, когда импорт возвращает объект с ключом default
+} catch (e) {
+	console.warn('menu.json not found, using default empty menu');
+	menu = []; // или какой-то fallback
+}
+
+menu.length > 0 && menu.map(item => {
+	if(typeof item?.children === 'string'){
+		const key = item.children;		
+		if (childrenGroup[key]) {			
+			item.children = childrenGroup[key];
+		}
+	}
+	
+});
+
+export const LINKS_MENU = menu;
 
 // Коллекции
 export const COLLECTIONS = [
