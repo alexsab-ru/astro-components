a0ea2ff7a1ad351774493860e54182fb241fc372
Alexander Sabanov <alexsab@mail.ru>
2025-09-10 11:16:30 +0400

    move content.config.ts to src

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import { defineCollection } from 'astro:content';
 import { glob, file } from 'astro/loaders';
-import { collectionSchema } from './src/content/collectionSchema.js';
-import { COLLECTIONS } from './src/const.js';
+import { collectionSchema } from './content/collectionSchema.js';
+import { COLLECTIONS } from './const.js';
 
 // Создаем коллекции из константы COLLECTIONS с новым API (loader: glob)
 const objectCollections = COLLECTIONS.reduce((acc: Record<string, ReturnType<typeof defineCollection>>, collection) => {
