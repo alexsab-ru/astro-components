2a85caac86efc709d41b9c36e8b1efae348df738
Alexander Sabanov <alexsab@mail.ru>
2025-09-23 07:12:49 +0400

    encode QUERY_STRING for URL

--- .github/scripts/sh/getDealerStorage.sh
+++ .github/scripts/sh/getDealerStorage.sh
@@ -58,8 +58,9 @@ if [ -z "$QUERY_STRING" ]; then
     fi
 fi
 
-# Кодируем QUERY_STRING для URL
-QUERY_STRING=$(echo "$QUERY_STRING" | sed 's/ /%20/g')
+# Кодируем QUERY_STRING для URL (правильное URL-кодирование)
+# Используем printf для корректного кодирования всех специальных символов
+QUERY_STRING=$(printf '%s\n' "$QUERY_STRING" | sed 's/ /%20/g; s/&/%26/g; s/=/%3D/g; s/+/%2B/g; s/,/%2C/g; s/;/%3B/g; s/:/%3A/g; s/?/%3F/g; s/#/%23/g; s/\[/%5B/g; s/\]/%5D/g; s/@/%40/g; s/!/%21/g; s/\$/%24/g; s/'"'"'/%27/g; s/(/%28/g; s/)/%29/g; s/*/%2A/g; s/+/%2B/g; s/,/%2C/g; s/;/%3B/g; s/=/%3D/g; s/?/%3F/g; s/@/%40/g; s/\[/%5B/g; s/\\/%5C/g; s/\]/%5D/g; s/\^/%5E/g; s/_/%5F/g; s/`/%60/g; s/{/%7B/g; s/|/%7C/g; s/}/%7D/g; s/~/%7E/g')
 
 # =====================
 # Дальнейший код использует уже выбранные переменные
