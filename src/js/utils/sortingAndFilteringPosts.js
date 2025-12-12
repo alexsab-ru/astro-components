2ef56db18dfbe9c110dd62c42971a30108bc9f2c
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-12-12 10:50:01 +0400

    Sorting by file name

--- src/js/utils/sortingAndFilteringPosts.js
+++ src/js/utils/sortingAndFilteringPosts.js
@@ -2,11 +2,25 @@ const nowWithTime = new Date();
 const nowWithoutTime = new Date();
 nowWithoutTime.setHours(0, 0, 0, 0);
 
+/**
+ * Извлекает номер из имени файла в filePath
+ * Например: "src/content/special-offers/1_novyy-haval-h3.mdx" -> 1
+ */
+function getFileNumber(post) {
+	if (!post?.filePath) return 0;
+	// Извлекаем имя файла из пути (последняя часть после '/')
+	const fileName = post.filePath.split('/').pop();
+	// Убираем расширение и берем первую часть до '_'
+	const fileNameWithoutExt = fileName.replace(/\.\w+$/, '');
+	const numberPart = fileNameWithoutExt.split('_')[0];
+	return parseInt(numberPart, 10) || 0;
+}
+
 export function sortingAndFilteringPosts(posts) {
 	return posts
 		// Исключаем файлы начинающиеся с '__'
 		.filter(post => !(typeof post?.id === 'string' && post.id.startsWith('__')))
-		.sort((a, b) => a.data.pubDate && b.data.pubDate ? b.data.pubDate.valueOf() - a.data.pubDate.valueOf() : parseInt(a.id.split('_')[0], 10) - parseInt(b.id.split('_')[0], 10))
+		.sort((a, b) => a.data.pubDate && b.data.pubDate ? b.data.pubDate.valueOf() - a.data.pubDate.valueOf() : getFileNumber(a) - getFileNumber(b))
 		.filter(post => !post.data.draft)
 		.filter(post => {
 			if (post.data?.toDate && typeof post.data?.toDate === 'object') {
