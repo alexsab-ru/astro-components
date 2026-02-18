62c756c73513ee098276876ae50b3269ac13df72
Alexander Sabanov <alexsab@mail.ru>
2026-02-18 16:32:28 +0400

    upd Advantages Item component
    added titleClass, textClass, iconColor, and fill default value in them
    added title on Icon tag

--- src/components/Advantages/types.ts
+++ src/components/Advantages/types.ts
@@ -1,7 +1,10 @@
 export type TAdvantagesItem = {
 	iconName?: string;
 	iconClass?: string;
+	iconColor?: string;
 	title: string;
+	titleClass?: string;
 	text?: string;
+	textClass?: string;
 	classes?: string;
 }
