fcd7fd3793e4f5c2968f8e3bcc4322b7f97af21c
Alexander Sabanov <alexsab@mail.ru>
2026-05-19 10:44:17 +0400

    add more legal documents

---
+++ src/js/utils/legalDocuments.js
@@ -0,0 +1,43 @@
+export const LEGAL_DOCUMENTS = [
+	{
+		key: 'privacyPolicy',
+		slug: 'privacy-policy',
+		href: '/privacy-policy/',
+		label: 'Политика конфиденциальности',
+	},
+	{
+		key: 'personalDataConsent',
+		slug: 'personal-data-consent',
+		href: '/personal-data-consent/',
+		label: 'Согласие на обработку персональных данных',
+	},
+	{
+		key: 'cookiePolicy',
+		slug: 'cookie-policy',
+		href: '/cookie-policy/',
+		label: 'Правила использования cookie',
+	},
+	{
+		key: 'advertisingConsent',
+		slug: 'advertising-consent',
+		href: '/advertising-consent/',
+		label: 'Согласие на рекламные сообщения',
+	},
+	{
+		key: 'termsOfUse',
+		slug: 'terms-of-use',
+		href: '/terms-of-use/',
+		label: 'Пользовательское соглашение',
+	},
+	{
+		key: 'thirdPartiesPage',
+		slug: 'third-parties',
+		href: '/third-parties/',
+		label: 'Третьи лица',
+	},
+];
+
+export const isLegalDocumentEnabled = (legal, key) => legal?.[key] === true;
+
+export const getEnabledLegalDocuments = (legal) =>
+	LEGAL_DOCUMENTS.filter((document) => isLegalDocumentEnabled(legal, document.key));
