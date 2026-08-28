41b25ff9477eb9cdda73e0b41fc5cc45fa73b7b1
Alexander Sabanov <alexsab@mail.ru>
2026-08-28 11:53:10 +0400

    CardMedia disable pagination when 1 slide

--- src/ui/card-media/slider.js
+++ src/ui/card-media/slider.js
@@ -5,18 +5,24 @@ const modelGallerySliders = document.querySelectorAll('.model-gallery-slider');
 
 if(modelGallerySliders.length){
     Array.from(modelGallerySliders).map(s => {
+        // Если слайд один, кнопки и пагинация не выводятся в разметке.
+        // Тогда слайдер инициализируем без навигации.
+        const hasControls = s.querySelectorAll('.swiper-slide').length > 1;
+
         new Swiper(s, {
             modules: [Navigation, Pagination],
             loop: true,
-            navigation: {
-                nextEl: ".model-gallery-slider-button-next",
-                prevEl: ".model-gallery-slider-button-prev",
-            },
-            pagination: {
-				el: '.simple-slider-pagination',
+            // Ищем элементы управления внутри текущего слайдера,
+            // иначе несколько галерей на странице делят одни и те же кнопки.
+            navigation: hasControls ? {
+                nextEl: s.querySelector(".model-gallery-slider-button-next"),
+                prevEl: s.querySelector(".model-gallery-slider-button-prev"),
+            } : false,
+            pagination: hasControls ? {
+				el: s.querySelector('.simple-slider-pagination'),
 				type: 'bullets',
 				clickable: true,
-			},
+			} : false,
         });
     })
 }
